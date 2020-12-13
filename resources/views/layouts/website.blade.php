f544d33e5a829ba4e10d28c9093417eb0e1b6711
Anis Dlakic <anis.dlakic@amplitudo.me>
2020-12-14 00:08:14 +0100

    fix

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -9,23 +9,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="/img/favicon.png" type="image/gif">
 
-    <link rel="stylesheet" href="{{ asset("theme/css/bootstrap.min.css") }}">
-    <link rel="stylesheet" href="{{ asset("theme/css/owl.carousel.min.css") }}">
-    <link rel="stylesheet" href="{{ asset("theme/css/magnific-popup.css") }}">
-    <link rel="stylesheet" href="{{ asset("theme/css/themify-icons.css") }}">
-    <link rel="stylesheet" href="{{ asset("theme/css/flaticon.css") }}">
-    <link rel="stylesheet" href="{{ asset("theme/css/gijgo.css") }}">
-    <link rel="stylesheet" href="{{ asset("theme/css/animate.min.css") }}">
-    <link rel="stylesheet" href="{{ asset("theme/css/slick.css") }}">
-    <link rel="stylesheet" href="{{ asset("theme/css/slicknav.css") }}">
-    <link rel="stylesheet" href="{{ asset("theme/css/style.css") }}">
+    <link rel="stylesheet" href="/theme/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/theme/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/theme/css/magnific-popup.css">
+    <link rel="stylesheet" href="/theme/css/themify-icons.css">
+    <link rel="stylesheet" href="/theme/css/flaticon.css">
+    <link rel="stylesheet" href="/theme/css/gijgo.css">
+    <link rel="stylesheet" href="/theme/css/animate.min.css">
+    <link rel="stylesheet" href="/theme/css/slick.css">
+    <link rel="stylesheet" href="theme/css/slicknav.css">
+    <link rel="stylesheet" href="theme/css/style.css">
 
-    <link rel="stylesheet" href="{{ mix("/css/app.css") }}">
+    <link rel="stylesheet" href="/css/app.css">
 
 </head>
 
 <body>
 
+<!--[if lte IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
+    your browser</a> to improve your experience and security.</p>
+<![endif]-->
 
 <div id="app2">
 
@@ -59,27 +63,27 @@
 
 <!--/ service_area_2  -->
 <!-- JS here -->
-<script src="{{ asset("theme/js/vendor/modernizr-3.5.0.min.js") }}"></script>
-<script src="{{ asset("theme/js/vendor/jquery-1.12.4.min.js") }}"></script>
-<script src="{{ asset("theme/js/popper.min.js") }}"></script>
-<script src="{{ asset("theme/js/bootstrap.min.js") }}"></script>
-<script src="{{ asset("theme/js/owl.carousel.min.js") }}"></script>
-<script src="{{ asset("theme/js/isotope.pkgd.min.js") }}"></script>
-<script src="{{ asset("theme/js/ajax-form.js") }}"></script>
-<script src="{{ asset("theme/js/waypoints.min.js") }}"></script>
-<script src="{{ asset("theme/js/jquery.counterup.min.js") }}"></script>
-<script src="{{ asset("theme/js/imagesloaded.pkgd.min.js") }}"></script>
-<script src="{{ asset("theme/js/scrollIt.js") }}"></script>
-<script src="{{ asset("theme/js/jquery.scrollUp.min.js") }}"></script>
-<script src="{{ asset("theme/js/wow.min.js") }}"></script>
-<script src="{{ asset("theme/js/jquery.slicknav.min.js") }}"></script>
-<script src="{{ asset("theme/js/jquery.magnific-popup.min.js") }}"></script>
-<script src="{{ asset("theme/js/plugins.js") }}"></script>
-<script src="{{ asset("theme/js/gijgo.min.js") }}"></script>
-<script src="{{ asset("theme/js/main.js") }}"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
-<script src="{{ asset("/js/app2.js") }}"></script>
+<script src="theme/js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="theme/js/vendor/jquery-1.12.4.min.js"></script>
+<script src="theme/js/popper.min.js"></script>
+<script src="theme/js/bootstrap.min.js"></script>
+<script src="theme/js/owl.carousel.min.js"></script>
+<script src="theme/js/isotope.pkgd.min.js"></script>
+<script src="theme/js/ajax-form.js"></script>
+<script src="theme/js/waypoints.min.js"></script>
+<script src="theme/js/jquery.counterup.min.js"></script>
+<script src="theme/js/imagesloaded.pkgd.min.js"></script>
+<script src="theme/js/scrollIt.js"></script>
+<script src="theme/js/jquery.scrollUp.min.js"></script>
+<script src="theme/js/wow.min.js"></script>
+<script src="theme/js/jquery.slicknav.min.js"></script>
+<script src="theme/js/jquery.magnific-popup.min.js"></script>
+<script src="theme/js/plugins.js"></script>
+<script src="theme/js/gijgo.min.js"></script>
+<script src="theme/js/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
+<script src="/js/app2.js"></script>
 
 @yield('scripts')
 
