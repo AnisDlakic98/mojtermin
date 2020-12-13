87f53908c9395235c0dfb127b7a012883d68c0ce
Anis Dlakic <anis.dlakic@amplitudo.me>
2020-12-14 00:09:21 +0100

    fix

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -17,8 +17,8 @@
     <link rel="stylesheet" href="/theme/css/gijgo.css">
     <link rel="stylesheet" href="/theme/css/animate.min.css">
     <link rel="stylesheet" href="/theme/css/slick.css">
-    <link rel="stylesheet" href="theme/css/slicknav.css">
-    <link rel="stylesheet" href="theme/css/style.css">
+    <link rel="stylesheet" href="/theme/css/slicknav.css">
+    <link rel="stylesheet" href="/theme/css/style.css">
 
     <link rel="stylesheet" href="/css/app.css">
 
@@ -63,24 +63,24 @@
 
 <!--/ service_area_2  -->
 <!-- JS here -->
-<script src="theme/js/vendor/modernizr-3.5.0.min.js"></script>
-<script src="theme/js/vendor/jquery-1.12.4.min.js"></script>
-<script src="theme/js/popper.min.js"></script>
-<script src="theme/js/bootstrap.min.js"></script>
-<script src="theme/js/owl.carousel.min.js"></script>
-<script src="theme/js/isotope.pkgd.min.js"></script>
-<script src="theme/js/ajax-form.js"></script>
-<script src="theme/js/waypoints.min.js"></script>
-<script src="theme/js/jquery.counterup.min.js"></script>
-<script src="theme/js/imagesloaded.pkgd.min.js"></script>
-<script src="theme/js/scrollIt.js"></script>
-<script src="theme/js/jquery.scrollUp.min.js"></script>
-<script src="theme/js/wow.min.js"></script>
-<script src="theme/js/jquery.slicknav.min.js"></script>
-<script src="theme/js/jquery.magnific-popup.min.js"></script>
-<script src="theme/js/plugins.js"></script>
-<script src="theme/js/gijgo.min.js"></script>
-<script src="theme/js/main.js"></script>
+<script src="/theme/js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="/theme/js/vendor/jquery-1.12.4.min.js"></script>
+<script src="/theme/js/popper.min.js"></script>
+<script src="/theme/js/bootstrap.min.js"></script>
+<script src="/theme/js/owl.carousel.min.js"></script>
+<script src="/theme/js/isotope.pkgd.min.js"></script>
+<script src="/theme/js/ajax-form.js"></script>
+<script src="/theme/js/waypoints.min.js"></script>
+<script src="/theme/js/jquery.counterup.min.js"></script>
+<script src="/theme/js/imagesloaded.pkgd.min.js"></script>
+<script src="/theme/js/scrollIt.js"></script>
+<script src="/theme/js/jquery.scrollUp.min.js"></script>
+<script src="/theme/js/wow.min.js"></script>
+<script src="/theme/js/jquery.slicknav.min.js"></script>
+<script src="/theme/js/jquery.magnific-popup.min.js"></script>
+<script src="/theme/js/plugins.js"></script>
+<script src="/theme/js/gijgo.min.js"></script>
+<script src="/theme/js/main.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
 <script src="/js/app2.js"></script>
