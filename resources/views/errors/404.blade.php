3f65ffbb573edb4944330ddc1787f433da4042d9
Anis Dlakic <anis.dlakic@amplitudo.me>
2020-12-14 03:47:07 +0100

    fix

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,16 @@
-<div class="data-not-found d-flex justify-content-center">
-    <div class="m-auto">
-        <img src="/img/broke.svg" class="w-50 mx-auto d-block tex-center" alt="">
-        <h2 class="mt-3 text-center">Stranica ne postoji!</h2>
-    </div>
-</div>
+@extends('layouts.website')
+
+@section('content')
+
+    <bradcam title="Greška 404"></bradcam>
+
+    <section class="section-padding">
+        <div class="data-not-found d-flex justify-content-center">
+            <div class="m-auto">
+                <img src="/img/broke.svg" class="w-50 mx-auto d-block tex-center" alt="">
+                <h2 class="mt-3 text-center">Stranica ne postoji!</h2>
+            </div>
+        </div>
+    </section>
+
+@endsection
